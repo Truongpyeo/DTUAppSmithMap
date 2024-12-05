c72ecf7f89e756d0822a18a51fab4e5701a19edd
Your Name <you@example.com>
2024-12-05 13:28:23 +0700

    New Commit

--- src/index.js
+++ src/index.js
@@ -1,41 +1,31 @@
 import L from 'leaflet';
 
 class DTUAppsmithMap {
-  constructor(containerId, options = {}) {
-    if (!document.getElementById('leaflet-css')) {
-      const link = document.createElement('link');
-      link.id = 'leaflet-css';
-      link.rel = 'stylesheet';
-      link.href = 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css';
-      link.integrity = 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=';
-      link.crossOrigin = '';
-      document.head.appendChild(link);
+    constructor(containerId, options = {}) {
+        this.map = null;
+        this.markers = [];
+        this.init(containerId, options);
     }
-    
-    this.map = null;
-    this.markers = [];
-    this.init(containerId, options);
-  }
 
-  init(containerId, options) {
-    // Khởi tạo map
-    this.map = L.map(containerId, options);
-    
-    // Thêm tile layer mặc định
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(this.map);
-  }
+    init(containerId, options) {
+        // Khởi tạo map
+        this.map = L.map(containerId, options);
 
-  // Custom methods
-  addMarker(lat, lng, options = {}) {
-    const marker = L.marker([lat, lng], options);
-    marker.addTo(this.map);
-    this.markers.push(marker);
-    return marker;
-  }
+        // Thêm tile layer mặc định
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(this.map);
+    }
 
-  // Thêm các method custom khác...
+    // Custom methods
+    addMarker(lat, lng, options = {}) {
+        const marker = L.marker([lat, lng], options);
+        marker.addTo(this.map);
+        this.markers.push(marker);
+        return marker;
+    }
+
+    // Thêm các method custom khác...
 }
 
 // Đảm bảo export default
